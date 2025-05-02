510fa83f8641850da478d1ab79fae501f773a515
Karl <167065682+KayoLolo@users.noreply.github.com>
2025-05-02 16:42:41 +0200

    new app

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,15 +4,34 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import TodoList from './TodoList'
 import Kanban from './Kanban'
+import { BrowserRouter, NavLink, Routes, Route } from "react-router-dom";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <TodoList />
-      <Kanban/>
-    </>
+    <BrowserRouter>
+        <div>
+          <nav>
+            <ul>
+              
+              <li>
+                <NavLink to="/TodoList">TodoList</NavLink>
+              </li>
+              <li>
+                <NavLink to="/Kanban">Kanban</NavLink>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path='/Todolist' element={<TodoList/>}></Route>
+            <Route path='/Kanban' element={<Kanban/>}></Route>
+            <Route path="*" element={<div>Page non trouvée</div>} />
+
+          </Routes>
+        </div>
+      </BrowserRouter>
   )
 }
 
