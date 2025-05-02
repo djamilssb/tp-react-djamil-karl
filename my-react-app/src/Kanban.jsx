fdf5d5627648503578eb0b68bc87b336a7b758bd
djamilssb <157473835+djamilssb@users.noreply.github.com>
2025-05-02 15:16:25 +0200

    x

--- my-react-app/src/Kanban.jsx
+++ my-react-app/src/Kanban.jsx
@@ -2,35 +2,7 @@ import React, { useState } from "react";
 import TodoList from "./TodoList";
 
 const Kanban = () => {
-    const [tasks, setTasks] = useState([
-        { id: 1, title: "Tâche 1", status: "To Do", assignee: "" },
-        { id: 2, title: "Tâche 2", status: "In Progress", assignee: "" },
-        { id: 3, title: "Tâche 3", status: "Done", assignee: "" },
-    ]);
-
-    const handleStatusChange = (id, newStatus) => {
-        setTasks((prevTasks) =>
-            prevTasks.map((task) =>
-                task.id === id ? { ...task, status: newStatus } : task
-            )
-        );
-    };
-
-    const handleAssigneeChange = (id, newAssignee) => {
-        setTasks((prevTasks) =>
-            prevTasks.map((task) =>
-                task.id === id ? { ...task, assignee: newAssignee } : task
-            )
-        );
-    };
-
-    return (
-        <div>
-            <h1>Gestion des Tâches</h1>
-            <TodoList tasks={tasks} onStatusChange={handleStatusChange} />
-            {/* Vous pouvez ajouter d'autres composants ou fonctionnalités ici */}
-        </div>
-    );
+    
 };
 
 export default Kanban;
